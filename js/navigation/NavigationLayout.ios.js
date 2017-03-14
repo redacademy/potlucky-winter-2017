d826684c6eae0884d8cbb19e74b5c2874a227438
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-14 10:57:47 -0700

    Styling tab-nav.

--- js/navigation/NavigationLayout.ios.js
+++ js/navigation/NavigationLayout.ios.js
@@ -2,22 +2,48 @@
  * @providesModule NavigationLayout
  * @flow
  */
-
 import {
   StackNavigator,
   TabNavigator
 } from 'react-navigation'
-
+import { Dimensions } from 'react-native'
 import SignInSignUpScreen from '../scenes/SignInSignUpScreen'
 import AccountScreen from '../scenes/AccountScreen'
 import MyPotLucksScreen from '../scenes/MyPotlucksScreen'
 import CreatePotluckScreen from '../scenes/CreatePotluckScreen'
+import { colors, typography } from '../styles/baseStyles'
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+const InputFoodTopTabNavigator = TabNavigator(
+  {
+    Info: { screen: AccountScreen },
+    Food: { screen: MyPotLucksScreen },
+  },
+  {
+    initialRouteName: 'Info',
+    tabBarPosition: 'top',
+    tabBarOptions: {
+      activeTintColor: 'white',
+      activeBackgroundColor: 'orange',
+      style: {
+        width: width*.9,
+        height:height*.04,
+        marginTop: 10,
+        borderRadius: 7,
+        alignSelf:'center',
+        overflow: 'hidden',
+        }
+      }
+    }
+)
 
 const HomeScreenNavigator = TabNavigator(
   {
     Account: { screen: AccountScreen },
     MyPotlucks: { screen: MyPotLucksScreen },
-    CreatePotluck: { screen: CreatePotluckScreen },
+    CreatePotluck: { screen: InputFoodTopTabNavigator },
   },
   {
     initialRouteName: 'Account',
