1aaaaaeff00e12926db5626655daebbc5d5e61d7
justinl-y <justin@sumofparts.ca>
2017-03-14 13:29:48 -0700

    Firebase schema modified

--- config/firebase.js
+++ config/firebase.js
@@ -6,7 +6,8 @@ export const firebaseUrl = 'https://potlucky-16935.firebaseio.com';
 export const firebaseConfig = {
   apiKey: "AIzaSyBjur9LPm-W7zZE_p4zOlKqYwsO3eviaP8",
   authDomain: "potlucky-16935.firebaseapp.com",
-  databaseURL: "https://potlucky-16935.firebaseio.com",
+  // databaseURL: "https://potlucky-16935.firebaseio.com",
+  databaseURL: firebaseUrl,
   storageBucket: "potlucky-16935.appspot.com",
   messagingSenderId: "821037068529"
 };
