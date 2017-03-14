38820c0545eadbf5dfcf946fd08e9c3c5c2ad089
Jonathan Funk <jonfunk88@hotmail.com>
2017-03-13 18:27:27 -0700

    fixed variable names

--- js/styles/baseStyles.js
+++ js/styles/baseStyles.js
@@ -7,7 +7,9 @@ export const colors = {
   mainBrandColor: '#FF8E3E',
   secondaryBrandColor: '#FFDD1E',
   tertiaryBrandColor: '#2EAEB7',
+  primaryBtnColor: '#FF8E3E',
   secondaryBtnColor: '#9DDA2C',
+  primaryNavColor: '#FF8E3E',
   secondaryNavColor: '#FFBC8C',
   appetizerColor: '#93B259',
   mainCourseColor: '#37B1B0',
@@ -16,9 +18,9 @@ export const colors = {
 }
 
 export const typography = {
-  smallestSize: '10px',
-  baseSize: '14px',
-  headerSize: '24px',
+  smallestFontSize: '10px',
+  baseFontSize: '14px',
+  headerFontSize: '24px',
   fontMain: 'Ubuntu-R.ttf',
   fontMainLight: 'Ubuntu-L.ttf',
   fontMainMedium: 'Ubuntu-M.ttf',
@@ -27,44 +29,44 @@ export const typography = {
 
 export default StyleSheet.create({
   headerPrimary: {
-    fontSize: headerSize,
-    fontFamily: fontMainMedium,
-    color: mainBrandColor,
+    fontSize: colors.headerSize,
+    fontFamily: colors.fontMainMedium,
+    color: colors.mainBrandColor,
   },
   headerSecondary: {
-    fontSize: headerSize,
-    fontFamily: fontMainMedium,
+    fontSize: typography.headerFontSize,
+    fontFamily: typography.fontMainMedium,
     color: 'white',
   },
   loginInput: {
-    fontSize: baseSize,
-    fontFamily: fontMainLight,
-    color: mainBrandColor,
+    fontSize: typography.baseFontSize,
+    fontFamily: typography.fontMainLight,
+    color: colors.mainBrandColor,
   },
   tutorialMain: {
     fontSize: '20px',
-    fontFamily: fontMain,
-    color: mainBrandColor,
+    fontFamily: typography.fontMain,
+    color: typography.mainBrandColor,
     lineHeight: '24px',
   },
   progressNumber: {
-    fontSize: baseSize,
-    fontFamily: fontMainMedium,
+    fontSize: typography.baseSize,
+    fontFamily: typography.fontMainMedium,
     color: 'white',
   },
   guestNumber: {
     fontSize: '50px',
-    fontFamily: fontMainMedium,
+    fontFamily: typography.fontMainMedium,
     color: 'black',
   },
   appetizerTitle: {
-    fontSize: baseSize,
-    fontFamily: fontMainBold,
-    color: mainBrandColor,
+    fontSize: typography.baseFontSize,
+    fontFamily: typography.fontMainBold,
+    color: colors.mainBrandColor,
   },
   mainCourseTitle: {
-    fontSize: baseSize,
-    fontfamily: fontMainBold,
-    color: mainBrand,
+    fontSize: typography.baseFontSize,
+    fontfamily: typography.fontMainBold,
+    color: colors.mainBrand,
   }
 });
