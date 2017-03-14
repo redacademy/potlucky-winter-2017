1a762e435fd253cf5f0aaf4eb11dbd37d8742072
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-14 09:31:16 -0700

    Navigation architecture skeleton

--- js/navigation/router.js
+++
@@ -1,9 +0,0 @@
-// import { createRouter, } from '@exponent/ex-navigation';
-
-// import Layout from './NavigationLayout';
-// import Hello from '../scenes/Hello';
-
-// export default createRouter(() => ({
-//   layout: () => Layout,
-//   hello: () => Hello
-// }));
