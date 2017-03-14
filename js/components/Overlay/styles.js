cabe58a3074a39d69ab82d7130b1c72a9a4c4e15
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-14 16:22:05 -0700

    imported screen dimensions and generalized class names

--- js/components/Overlay/styles.js
+++ js/components/Overlay/styles.js
@@ -1,22 +1,24 @@
-import { StyleSheet, Dimensions } from 'react-native'
-import { typography } from '../../styles/baseStyles'
+import { StyleSheet } from 'react-native'
+import { typography, windowHeight as height, windowWidth as width } from '../../styles/baseStyles'
+
+const highlightedTextColor ='#FF8E3E'
 
 export default StyleSheet.create({
   contentContainer: {
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width - 100,
+    height,
+    width: width -100,
     alignItems: 'center',
     paddingTop: 150,
   },
   modalView: {
     backgroundColor: 'rgba(0, 0, 0, 0.6)',
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width,
+    height,
+    width,
     paddingTop: 25,
     alignItems: 'center'
   },
   closeButtonView: {
-    width: Dimensions.get('window').width,
+    width,
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
@@ -25,18 +27,12 @@ export default StyleSheet.create({
     width: 20,
     marginRight: 15
   },
-  whiteText: {
+  highlightedText: {
+    color: highlightedTextColor,
+  },
+  baseTextStyling: {
     color: 'white',
     fontSize: 20,
-    fontFamily: typography.fontMain,
-    fontWeight: '500',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  orangeText: {
-    color: 'orange',
-    fontSize: 25,
-    fontFamily: typography.fontMain,
     fontWeight: '500',
     textAlign: 'center',
     marginBottom: 20,
