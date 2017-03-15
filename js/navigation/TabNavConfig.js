d4d9ddf3b1b6684ab4e0ef2400e9d5911e150e6e
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-14 18:51:47 -0700

    Fix typo.

--- js/navigation/TabNavConfig.js
+++ js/navigation/TabNavConfig.js
@@ -12,7 +12,7 @@ export const hostTabScreens = {
 export const hostTabStyles = {
   initialRouteName: 'Info',
   tabBarPosition: 'top',
-  tabBarOptions: {
+  tabBarOptions: { 
     activeTintColor: 'white',
     inactiveTintColor: colors.mainBrandColor,
     activeBackgroundColor: colors.mainBrandColor,
