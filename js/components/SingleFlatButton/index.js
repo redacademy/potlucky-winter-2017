49e4ce42e48fa818aa3953e9283af69d2caa1a45
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-17 14:12:28 -0700

    Changed constants to props, flat button.

--- js/components/SingleFlatButton/index.js
+++ js/components/SingleFlatButton/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { View, Button } from 'react-native';
 import styles from './styles';
 
-const SingleFlatButton = () => {
+const SingleFlatButton = ({ title, color }) => {
   return (
     <View style={styles.buttonContainer}>
-      <Button title="Looks Good" color="white" />
+      <Button title={title} color={color} />
     </View>
   );
+};
+SingleFlatButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired
 };
 
 export default SingleFlatButton;
