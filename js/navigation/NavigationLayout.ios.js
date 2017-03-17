264cbb97cb39df47b450e513ba978bc3a75d0ff7
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-17 10:18:31 -0700

    moved create potluck flow into a tab navigator from stack

--- js/navigation/NavigationLayout.ios.js
+++ js/navigation/NavigationLayout.ios.js
@@ -5,23 +5,26 @@
 import {
   StackNavigator,
   TabNavigator
-} from 'react-navigation'
-import PotLuckInfoScreen from '../scenes/PotLuckInfoScreen'
-import SignInSignUpScreen from '../scenes/SignInSignUpScreen'
-import AccountScreen from '../scenes/AccountScreen'
-import MyPotLucksScreen from '../scenes/MyPotlucksScreen'
-import CreateNewPotLuck from './../scenes/CreatePotluckScreen'
-import NumberOfGuestScreen from './../scenes/NumberOfGuestScreen'
-import FoodPlanningScreen from './../scenes/FoodPlanningScreen'
-import InvitesScreen from '../scenes/InvitesScreen'
-import { hostTabScreens, hostTabStyles, guestTabScreens, guestTabStyles } from './TabNavConfig'
-import { bottomNavigatorScreens, bottomNavigatorStyles } from './BottomNavConfig'
+} from 'react-navigation';
+import PotLuckInfoScreen from '../scenes/PotLuckInfoScreen';
+import SignInSignUpScreen from '../scenes/SignInSignUpScreen';
+import AccountScreen from '../scenes/AccountScreen';
+import MyPotLucksScreen from '../scenes/MyPotlucksScreen';
+import CreateNewPotLuck from './../scenes/CreatePotluckScreen';
+import NumberOfGuestScreen from './../scenes/NumberOfGuestScreen';
+import FoodPlanningScreen from './../scenes/FoodPlanningScreen';
+import InvitesScreen from '../scenes/InvitesScreen';
+import InfoScreen from '../scenes/InfoScreen';
+import { hostTabScreens, hostTabStyles, guestTabScreens, guestTabStyles } from './TabNavConfig';
+import { bottomNavigatorScreens, bottomNavigatorStyles } from './BottomNavConfig';
+
+import { colors, windowWidth } from '../styles/baseStyles'
 
 const HostTabNavigator = TabNavigator(hostTabScreens, hostTabStyles);
 
 const GuestTabNavigator = TabNavigator(guestTabScreens, guestTabStyles);
 
-const BottomNavigator = TabNavigator(bottomNavigatorScreens, bottomNavigatorStyles)
+const BottomNavigator = TabNavigator(bottomNavigatorScreens, bottomNavigatorStyles);
 
 const HomeScreenNavigator = TabNavigator(
   {
@@ -32,24 +35,42 @@ const HomeScreenNavigator = TabNavigator(
   {
     initialRouteName: 'Account',
   }
-)
+);
 
 
 BottomNavigator.navigationOptions = {
 
 };
 
-export default StackNavigator(
+
+const CreatePotluckNavigator = TabNavigator(
   {
-    InvitesScreen: { screen: InvitesScreen },
-    PotLuckInfoScreen: { screen: PotLuckInfoScreen },
-    Login: { screen: SignInSignUpScreen },
-    HomeScreenNavigator: { screen: HomeScreenNavigator },
     NumberOfGuestScreen: { screen: NumberOfGuestScreen },
     FoodPlanningScreen: { screen: FoodPlanningScreen },
+    PotLuckInfoScreen: { screen: PotLuckInfoScreen },
+    InvitesScreen: { screen: InvitesScreen },
+  },
+  {
+    initialRouteName: 'NumberOfGuestScreen',
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarOptions: {
+      style: { height: 0 }
+    }
+  }
+);
+
+export default StackNavigator(
+  {
+
+    PotLuckInfoScreen: { screen: PotLuckInfoScreen },
+    Login: { screen: SignInSignUpScreen },
+    CreatePotluckNavigator: { screen: CreatePotluckNavigator },
+    HomeScreenNavigator: { screen: HomeScreenNavigator },
+
   },
   {
     initialRouteName: 'Login',
-    headerMode: 'screen'
+
   }
-)
+);
