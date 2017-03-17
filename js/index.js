01241ef7dafbabc16ff45c883d9e58941d7bfcb6
justinl-y <justin@sumofparts.ca>
2017-03-17 13:40:34 -0700

    Firebase config changed

--- js/index.js
+++ js/index.js
@@ -3,17 +3,9 @@ import { Provider, } from 'react-redux';
 import firebase from 'firebase';
 import Navigation from './navigation/NavigationLayout.ios';
 import Store from './redux/store';
+import { firebaseConfig } from '../config/firebase';
 
 // Initialize Firebase
-const firebaseUrl = 'https://potlucky-16935.firebaseio.com';
-const firebaseConfig = {
-  apiKey: 'AIzaSyBjur9LPm-W7zZE_p4zOlKqYwsO3eviaP8',
-  authDomain: 'potlucky-16935.firebaseapp.com',
-  databaseURL: firebaseUrl,
-  storageBucket: 'potlucky-16935.appspot.com',
-  messagingSenderId: '821037068529',
-};
-
 firebase.initializeApp(firebaseConfig);
 
 export default class Potlucky extends Component {
