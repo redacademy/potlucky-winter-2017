cba1d93e749e7bb1d8439e830a2531a8419a928b
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-17 10:20:18 -0700

    styled

---
+++ js/scenes/PotLuckInfoScreen/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import { colors, typography, windowWidth, windowHeight } from './../../styles/baseStyles'
+
+
+export default StyleSheet.create({
+  container: {
+    flex: 0,
+    marginTop: 200,
+    width: windowWidth,
+    height: windowHeight-200,
+  },
+});
