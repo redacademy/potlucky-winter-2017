cf3b8c8c48697515ecd96bc6df6076b21e9f9955
Jonathan Funk <jonfunk88@hotmail.com>
2017-03-20 13:32:17 -0700

    shortened add potluck item functionality

--- js/redux/modules/newPotluckReducer.js
+++ js/redux/modules/newPotluckReducer.js
@@ -19,65 +19,11 @@ export default (state = newPotluckInitialState, action) => {
       }
     case 'ADD_POTLUCK_ITEM':
       if (state.guestNumber) {
-        switch (action.payload) {
-          case 'Appetizers':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Appetizers: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'Mains':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Mains: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'Salads':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Salads: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'Desserts':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Desserts: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'Drinks':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Drinks: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'ChipIn':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                ChipIn: state.potluckFood[action.payload] + 1
-              }
-            }
-          case 'Talent':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Talent: state.potluckFood[action.payload] + 1
-              }
-            }    
-          case 'Custom':
-            return { ...state,
-              guestNumber: state.guestNumber - 1,
-              potluckFood: { ...state.potluckFood,
-                Custom: state.potluckFood[action.payload] + 1
-              }
-            }                                                                   
-          default:
-            return state;
+        return { ...state,
+          guestNumber: state.guestNumber - 1,
+          potluckFood: { ...state.potluckFood,
+            [action.payload]: state.potluckFood[action.payload] + 1
+          }
         }
       }
     default:
