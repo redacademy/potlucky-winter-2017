e10c7fb6fcb0a8de8df6e718097d7151478b9d62
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-20 10:21:56 -0700

    merged

--- js/navigation/NavigationLayout.ios.js
+++ js/navigation/NavigationLayout.ios.js
@@ -5,22 +5,6 @@
 import {
   StackNavigator,
   TabNavigator
-<<<<<<< HEAD
-} from 'react-navigation';
-import PotLuckInfoScreen from '../scenes/PotLuckInfoScreen';
-import SignInSignUpScreen from '../scenes/SignInSignUpScreen';
-import AccountScreen from '../scenes/AccountScreen';
-import MyPotLucksScreen from '../scenes/MyPotlucksScreen';
-import CreateNewPotLuck from './../scenes/CreatePotluckScreen';
-import NumberOfGuestScreen from './../scenes/NumberOfGuestScreen';
-import FoodPlanningScreen from './../scenes/FoodPlanningScreen';
-import InvitesScreen from '../scenes/InvitesScreen';
-import InfoScreen from '../scenes/InfoScreen';
-import { hostTabScreens, hostTabStyles, guestTabScreens, guestTabStyles } from './TabNavConfig';
-import { bottomNavigatorScreens, bottomNavigatorStyles } from './BottomNavConfig';
-
-import { colors, windowWidth } from '../styles/baseStyles'
-=======
 } from 'react-navigation'
 import PotLuckInfoScreen from '../scenes/PotLuckInfoScreen'
 import CreateNewPotLuck from './../scenes/CreatePotluckScreen'
@@ -33,7 +17,6 @@ import AccountScreen from '../scenes/AccountScreen';
 import MyPotLucksScreen from '../scenes/MyPotlucksScreen';
 import { bottomNavigatorScreens, bottomNavigatorStyles } from './BottomNavConfig'
 import { hostTabScreens, hostTabStyles, guestTabScreens, guestTabStyles } from './TabNavConfig';
->>>>>>> origin/develop
 
 const HostTabNavigator = TabNavigator(hostTabScreens, hostTabStyles);
 
@@ -80,20 +63,12 @@ export default StackNavigator(
 
     PotLuckInfoScreen: { screen: PotLuckInfoScreen },
     Login: { screen: SignInSignUpScreen },
-<<<<<<< HEAD
-    CreatePotluckNavigator: { screen: CreatePotluckNavigator },
-=======
     SignUp: { screen: SignUp },
->>>>>>> origin/develop
     HomeScreenNavigator: { screen: HomeScreenNavigator },
 
   },
   {
     initialRouteName: 'Login',
-<<<<<<< HEAD
-
-=======
     headerMode: 'screen',
->>>>>>> origin/develop
   }
 );
