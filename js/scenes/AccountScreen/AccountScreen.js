9eb28975ddf9ca2a67734dc8c69a7fc3b0aac572
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-20 10:11:03 -0700

    Completed layout for account page.

--- js/scenes/AccountScreen/AccountScreen.js
+++ js/scenes/AccountScreen/AccountScreen.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import {
+  TextInput,
   Text,
   View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 import styles from './styles';
 
-const AccountScreen = () => {
+const AccountScreen = ({ firstName, lastName, email, password }) => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../../../assets/images/account.png')} />
+      </View>
+      <View style={styles.formContainer}>
+        <View style={styles.formsBox}>
+          <View style={styles.formElementContainer}>
+            <TextInput placeholder={firstName} style={styles.formElement} />
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.button}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.formElementContainer}>
+            <TextInput placeholder={lastName} style={styles.formElement} />
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.button}>Edit</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.formElementContainer}>
+            <TextInput placeholder={email} style={styles.formElement} />
+          </View>
+          <View style={styles.formElementContainer}>
+            <TextInput placeholder={password} style={styles.formElement} />
+          </View>
+        </View>
+      </View>
     </View>
   );
+};
+
+AccountScreen.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
 
 export default AccountScreen;
--- js/scenes/AccountScreen/styles.js
+++ js/scenes/AccountScreen/styles.js
@@ -1,12 +1,56 @@
-import { StyleSheet } from 'react-native'
-
+import { StyleSheet } from 'react-native';
+import { colors, typography, windowWidth as width, windowHeight as height } from './../../styles/baseStyles';
 
 export default StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
+    backgroundColor: 'white'
   },
-  placeholderText: {
+  imageContainer: {
+    flex: 0,
+    justifyContent: 'center',
+    height: height * 0.30,
+  },
+  image: {
+    alignSelf: 'center',
+    height: 145,
+    width: 145
+  },
+  formContainer: {
     flex: 1,
-    marginTop: 100,
-  }
+    justifyContent: 'center',
+    height: height * 0.30,
+  },
+  formsBox: {
+    alignSelf: 'center',
+    width: width * 0.80,
+    paddingBottom: 20,
+    height: height * 0.40
+  },
+  buttonContainer: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    height: 16,
+    width: 50,
+    paddingLeft: 10,
+    paddingTop: 5,
+    backgroundColor: 'transparent'
+  },
+  button: {
+    fontSize: typography.baseFontSize,
+    color: '#FF8E3E',
+    textAlign: 'center'
+  },
+  formElementcontainer: {
+    flex: 1,
+    position: 'relative',
+    justifyContent: 'center'
+  },
+  formElement: {
+    height: 30,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: colors.borderColorGray,
+    borderRadius: 3,
+  },
 });
