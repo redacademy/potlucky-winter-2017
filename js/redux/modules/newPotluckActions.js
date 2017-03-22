4492c424e526a84204210a8c4cd170b0fdba6d5d
Djordje Vujatovic <djordje_v@hotmail.com>
2017-03-22 09:43:55 -0700

    Fixing issue.

--- js/redux/modules/newPotluckActions.js
+++ js/redux/modules/newPotluckActions.js
@@ -1,9 +1,16 @@
-export const changeNumberofGuests = number => ({
-  type: 'changeNumberofGuests',
-  payload: number
-});
+// actions
 
-export const addPotluckItem = name => ({
-  type: 'ADD_POTLUCK_ITEM',
-  payload: name
-});
+const ADD_GUESTS = 'ADD_GUESTS';
+const ADD_FOOD = 'ADD_FOOD';
+const ADD_INFO = 'ADD_INFO';
+const ADD_INVITES = 'ADD_INVITES';
+
+// action creators
+
+export const addGuests = data => ({ type: ADD_GUESTS, payload: data });
+export const addFood = data => ({ type: ADD_FOOD, payload: data });
+export const addInfo = data => ({ type: ADD_INFO, payload: data });
+export const addInvited = data => ({ type: ADD_INVITES, payload: data });
+
+
+
--- js/redux/modules/newPotluckReducer.js
+++ js/redux/modules/newPotluckReducer.js
@@ -1,32 +1,39 @@
-const newPotluckInitialState = {
-  guestNumber: 15,
-  potluckFood: {
-    Appetizers: 0,
-    Mains: 0,
-    Salads: 0,
-    Desserts: 0,
-    Drinks: 0,
-    ChipIn: 0,
-    Talent: 0,
-    Custom: 0,
-  }
-}
-export default (state = newPotluckInitialState, action) => {
+// initial state
+
+const initialState = {
+  potLucks: {
+    userId: null,
+    title: null,
+    theme: null,
+    eventDate: null,
+    arriveTime: null,
+    serveTime: null,
+    location: null,
+    description: null
+  },
+  potLuckFood: {
+    dishCount: 0
+  },
+  potLuckInvites: {
+    emails: []
+  },
+};
+
+// reducer
+
+const newPotluckReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'changeNumberofGuests':
-      return { ...state,
-        guestNumber: action.payload
-      }
-    case 'ADD_POTLUCK_ITEM':
-      if (state.guestNumber) {
-        return { ...state,
-          guestNumber: state.guestNumber - 1,
-          potluckFood: { ...state.potluckFood,
-            [action.payload]: state.potluckFood[action.payload] + 1
-          }
-        }
-      }
+    case 'ADD_GUESTS':
+      return { ...state, dishCount: action.payload.potLuckFood.dishCount };
+    case 'ADD_FOOD':
+      return { ...state, potLuckFood: action.payload.potLuckFood };
+    case 'ADD_INFO':
+      return { ...state, potLuckInfo: action.payload.potLucks };
+    case 'ADD_INVITES':
+      return { ...state, potLuckInvites: action.payload.potLuckInvites };
     default:
       return state;
   }
-}
+};
+
+export default newPotluckReducer;
