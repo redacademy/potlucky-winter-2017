fe797af93164295f71f3b26e5de4eff80fab92d6
Ringo <ringong95@gmail.com>
2017-03-23 09:57:29 -0700

    Added Props for conditional rendering of content.

--- js/components/PotluckConfirmationInfo/index.js
+++ js/components/PotluckConfirmationInfo/index.js
@@ -3,48 +3,47 @@ import { Text, View, Image, ScrollView } from 'react-native';
 import SingleFlatButton from '../SingleFlatButton';
 import styles from './styles';
 
-const buttonRendering = (buttonType) => {
-
+const buttonRendering = (buttonType, mainButtonTitle, secButtonTitle) => {
   if (buttonType === 'rsvp') {
     return (
       <View style={styles.buttonContainer}>
-        <SingleFlatButton width={'half'} style={styles.rsvpButton} title={'Sounds Good'} />
-        <SingleFlatButton width={'half'} style={styles.rsvpButton} title={'Maybe Next Time'} />
+        <SingleFlatButton width={'half'} style={styles.rsvpButton} title={mainButtonTitle} />
+        <SingleFlatButton width={'half'} style={styles.rsvpButton} title={secButtonTitle} />
       </View>
     );
   }
   if (buttonType === 'confirmation') {
-    return <SingleFlatButton title={'Confirm'} color="green" />;
+    return <SingleFlatButton title={mainButtonTitle} color="green" />;
   }
   return null;
 };
 
-const PotluckConfirmationInfo = ({ title, color, buttonNeeded = true, buttonType }) => (
+const PotluckConfirmationInfo = ({ location, date, servingTime, arrivingTime, title, image, color, buttonNeeded = true, buttonType, mainButtonTitle, secButtonTitle, description }) => (
   <View style={styles.container}>
     <View style={styles.inputContainer}>
       <ScrollView style={styles.scrollViewContainer}>
         <View style={styles.imageContainer}>
-          <Image style={styles.image} source={require('../../../assets/images/southparklastsupper.jpg')} />
+          <Image style={styles.image} source={image} />
           <View style={styles.textBanner}>
-            <Text style={styles.bannerText}> South Park: The Last Supper</Text>
+            <Text style={styles.bannerText}> {title}</Text>
           </View>
         </View>
         <View style={styles.infoBox}>
           <View style={styles.textContainer}>
             <Text style={styles.colorText}>Date: </Text>
-            <Text>February 14, 2020</Text>
+            <Text>{date}</Text>
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.colorText}>Arriving Time: </Text>
-            <Text> 12:30PM</Text>
+            <Text> {arrivingTime}</Text>
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.colorText}>Serving Time: </Text>
-            <Text>1:30PM</Text>
+            <Text>{servingTime}</Text>
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.colorText}>Location: </Text>
-            <Text>Belgrade, Serbia</Text>
+            <Text> {location}</Text>
           </View>
         </View>
         <View>
@@ -53,15 +52,14 @@ const PotluckConfirmationInfo = ({ title, color, buttonNeeded = true, buttonType
         <View style={styles.descriptionBox}>
           <Text style={styles.descriptionHeader}>Description</Text>
           <Text style={styles.descriptionText}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            {description}
           </Text>
         </View>
       </ScrollView>
     </View>
     {
       buttonNeeded &&
-      buttonRendering(buttonType)
+      buttonRendering(buttonType, mainButtonTitle, secButtonTitle)
     }
   </View>
 
--- js/scenes/InfoScreen/InfoScreen.js
+++ js/scenes/InfoScreen/InfoScreen.js
@@ -7,7 +7,21 @@ import PotluckConfirmationInfo from './../../components/PotluckConfirmationInfo'
 import styles from './styles';
 
 const InfoScreen = () => (
-  <PotluckConfirmationInfo title={'Submit'} color={'red'} buttonNeeded={true} buttonType={'rsvp'} />
+  <PotluckConfirmationInfo
+    title={'Submit'}
+    color={'red'}
+    buttonNeeded={true}
+    buttonType={'rsvp'}
+    image={require('../../../assets/images/southparklastsupper.jpg')}
+    date={'February 14, 2020'}
+    arrivingTime={'12:30PM'}
+    servingTime={'1:30PM'}
+    location={'Belgrade, Serbia'}
+    mainButtonTitle={'Sounds good!'}
+    secButtonTitle={'Maybe next time!'}
+    description={` Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`}
+  />
 );
 
 export default InfoScreen;
