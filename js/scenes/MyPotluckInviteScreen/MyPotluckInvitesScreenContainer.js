2006bbab45f942296eeb1431b584b3000303b43f
Ringo <ringong95@gmail.com>
2017-03-27 15:25:53 -0700

    Changed MyPotlucks back arrow to component.

--- js/scenes/MyPotluckInviteScreen/MyPotluckInvitesScreenContainer.js
+++ js/scenes/MyPotluckInviteScreen/MyPotluckInvitesScreenContainer.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
 import MyPotluckInvitesScreen from './MyPotluckInvitesScreen';
 import { colors } from '../../styles/baseStyles';
+import NavigationArrow from './../../components/NavigationArrow';
 
 class MyPotluckInvitesScreenContainer extends Component {
   static navigationOptions = {
-    header: () => ({
+    header: ({ dispatch }) => ({
+      left: <NavigationArrow
+        onPress={() => (
+          dispatch(NavigationActions.back())
+        )}
+        backArrow
+      />,
       style: { backgroundColor: colors.mainBrandColor }
-    })
-  };
+    }),
+  }
   render() {
     return (
       <MyPotluckInvitesScreen />
