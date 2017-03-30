5c135c8dba6fd174bc1d161a3e375de0b678436c
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-30 16:45:04 -0700

    commit

--- js/scenes/GuestPotluckInfoScreen/GuestPotluckInfoScreen.js
+++ js/scenes/GuestPotluckInfoScreen/GuestPotluckInfoScreen.js
@@ -6,7 +6,7 @@ import styles from './styles';
 import { colors } from '../../styles/baseStyles';
 import SingleFlatButton from '../../components/SingleFlatButton';
 
-const GuestPotluckInfoScreen = ({ currentPotluck, actionInvite }) => (
+const GuestPotluckInfoScreen = ({ currentPotluck, actionInvite, message }) => (
   <View style={styles.container}>
     <PotluckInfo
       title={currentPotluck.title}
@@ -20,19 +20,30 @@ const GuestPotluckInfoScreen = ({ currentPotluck, actionInvite }) => (
     />
 
     {
-      !currentPotluck.isNew &&
-      <View style={styles.buttonContainer}>
-        <SingleFlatButton
-          title={'Sorry, can\'t make it'}
-          onPress={() => actionInvite({ inviteSelection: 'inviteDeclined' })}
-          backgroundColor={colors.invitePrimaryBtn}
-        />
-        <SingleFlatButton
-          title={'See you there!'}
-          onPress={() => actionInvite({ inviteSelection: 'inviteAccepted' })}
-          backgroundColor={colors.inviteSecondaryBtn}
-        />
-      </View>
+      message ?
+        <View style={styles.buttonContainer}>
+          <SingleFlatButton
+            title={'Thank you, for your response!'}
+            onPress={(console.log('yay'))}
+            backgroundColor={colors.invitePrimaryBtn}
+          />
+        </View>
+        :
+        (
+          !currentPotluck.isNew &&
+          <View style={styles.buttonContainer}>
+            <SingleFlatButton
+              title={'Sorry, can\'t make it'}
+              onPress={() => actionInvite({ inviteSelection: 'inviteDeclined' })}
+              backgroundColor={colors.invitePrimaryBtn}
+            />
+            <SingleFlatButton
+              title={'See you there!'}
+              onPress={() => actionInvite({ inviteSelection: 'inviteAccepted' })}
+              backgroundColor={colors.inviteSecondaryBtn}
+            />
+          </View>
+        )
     }
 
   </View >
